fa2b980913d0f01dd967cd336d0a0d75a2babe18
CloverInc <shpyta-gleb@mail.ru>
2015-09-08 17:35:41 +0300

    Email send and scan v1.0

--- Crypto/Crypto/MainWindow.xaml.cs
+++ Crypto/Crypto/MainWindow.xaml.cs
@@ -7,6 +7,15 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -34,24 +43,23 @@ namespace Crypto
         {
             InitializeComponent();
         }
-        
-        
+
+
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             cbCodeName.Items.Add("Caesar");
-            cbCodeName.Items.Add("Triterius");            
         }
 
         private void Send(string Text)
         {
-            SmtpClient Smtp = new SmtpClient("smtp.mail.ru", 25);
-            Smtp.Credentials = new NetworkCredential("shpyta-gleb@mail.ru", "utybfkmyjcnm321");
+            SmtpClient Smtp = new SmtpClient("smtp.yandex.ru", 25);
+            Smtp.Credentials = new NetworkCredential("khapilins@yandex.ru", "271044kot");
             Smtp.EnableSsl = true;
 
             MailMessage message = new MailMessage();
-            message.From = new System.Net.Mail.MailAddress("shpyta-gleb@mail.ru");
-            message.To.Add(new System.Net.Mail.MailAddress("khapilins@yandex.ru"));
+            message.From = new System.Net.Mail.MailAddress("khapilins@yandex.ru");
+            message.To.Add(new System.Net.Mail.MailAddress("shpyta-gleb@mail.ru"));
             message.Subject = "Test";
             message.Body = Text;
 
@@ -60,34 +68,41 @@ namespace Crypto
 
         private void Parse()
         {
-                TcpClient tcpclient = new TcpClient(); // create an instance of TcpClient
-                tcpclient.Connect("pop.gmail.com", 995); // HOST NAME POP SERVER and gmail uses port number 995 for POP 
-                System.Net.Security.SslStream sslstream = new SslStream(tcpclient.GetStream()); // This is Secure Stream // opened the connection between client and POP Server
-                sslstream.AuthenticateAsClient("pop.gmail.com"); // authenticate as client 
-                //bool flag = sslstream.IsAuthenticated; // check flag 
-                System.IO.StreamWriter sw = new StreamWriter(sslstream); // Asssigned the writer to stream
-                System.IO.StreamReader reader = new StreamReader(sslstream); // Assigned reader to stream
-                sw.WriteLine("shpytagleb@gmail.com"); // refer POP rfc command, there very few around 6-9 command
-                sw.Flush(); // sent to server
-                sw.WriteLine("utybfkmyjcnm");
-                sw.Flush();
-                sw.WriteLine("5"); 
-                sw.Flush();
-                sw.WriteLine("Quit "); // close the connection
-                sw.Flush();
-                string str = string.Empty;
-                string strTemp = string.Empty;
+            TcpClient tcpclient = new TcpClient(); // create an instance of TcpClient
+            tcpclient.Connect("pop.mail.ru", 995); // HOST NAME POP SERVER and gmail uses port number 995 for POP 
+            System.Net.Security.SslStream sslstream = new SslStream(tcpclient.GetStream()); // This is Secure Stream // opened the connection between client and POP Server
+            sslstream.AuthenticateAsClient("pop.mail.ru"); // authenticate as client 
+            //bool flag = sslstream.IsAuthenticated; // check flag 
+            System.IO.StreamWriter sw = new StreamWriter(sslstream); // Asssigned the writer to stream
+            System.IO.StreamReader reader = new StreamReader(sslstream); // Assigned reader to stream
+            sw.WriteLine("USER shpyta-gleb@mail.ru"); // refer POP rfc command, there very few around 6-9 command
+            sw.Flush(); // sent to server
+            sw.WriteLine("PASS utybfkmyjcnm321");
+            sw.Flush();
+            sw.WriteLine("RETR 5");
+            sw.Flush();
+            sw.WriteLine("Quit "); // close the connection
+            sw.Flush();
+            string str = string.Empty;
+            string strTemp = string.Empty;
 
-                while ((strTemp = reader.ReadLine()) != null)
+            while ((strTemp = reader.ReadLine()) != null)
+            {
+                if (".".Equals(strTemp))
                 {
-
-                    str += strTemp;
+                    break;
                 }
-
-                MessageBox.Show(str); 
+                if (strTemp.IndexOf("-ERR") != -1)
+                {
+                    break;
+                }
+                str += strTemp;
             }
 
-             
+            MessageBox.Show(str);
+        }
+
+
 
 
         private void ButtonEncrypt_Click(object sender, RoutedEventArgs e)
@@ -103,12 +118,17 @@ namespace Crypto
                 MessageBox.Show(messageToSend);
             }
             else MessageBox.Show("Choose CryptoClass");
-            
+
         }
 
         private void ButtonDecrypt_Click(object sender, RoutedEventArgs e)
         {
             Parse();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Send("kawaii");
         }
 
 
